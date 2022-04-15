4a15560693e457cc5fa05431c43e25af78c6b4c5
Babajide Akinseye <bakinseye247@gmail.com>
2022-04-15 22:30:56 +0100

    upload file(package.json)

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const session = require("express-session");
 const LocalStrategy = require("passport-local");
 const passportLocalMongoose = require("passport-local-mongoose");
 const passport = require("passport");
+const req = require("express/lib/request");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +21,19 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      console.log(username)
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+    
+  }
+));
 
 mongoose.connect("mongodb://localhost:27017/UserDB");
 
@@ -66,9 +80,12 @@ app.post("/register", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-
+    const username = req.body.username
+    const password = req.body.password
   await passport.authenticate("local", { failureRedirect: "/login" }),
     res.redirect("/secrets");
+
+    console.log(username)
 });
 
 app.listen(3000);
